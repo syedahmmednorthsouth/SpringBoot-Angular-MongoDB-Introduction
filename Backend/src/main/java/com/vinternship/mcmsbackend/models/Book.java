a5843a485e1ff2eb022a44e2a27c6ad05a21b256
syedahmmednorthsouth <syed.ahmmed@northsouth.edu>
2022-02-06 05:00:48 +0600

    Book And BookShop Model added

---
+++ Backend/src/main/java/com/vinternship/mcmsbackend/models/Book.java
@@ -0,0 +1,97 @@
+package com.vinternship.mcmsbackend.models;
+
+import org.springframework.data.annotation.Id;
+
+import java.time.Year;
+
+public class Book {
+
+    @Id
+    private  String id;
+    private String title;
+    private double price;
+    private Year year_of_publishing;
+    private Author author;
+    private String genre;
+    private String publisher;
+
+
+    public Book(String id, String title, double price, Year year_of_publishing, Author author, String genre, String publisher) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.year_of_publishing = year_of_publishing;
+        this.author = author;
+        this.genre = genre;
+        this.publisher = publisher;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Year getYear_of_publishing() {
+        return year_of_publishing;
+    }
+
+    public void setYear_of_publishing(Year year_of_publishing) {
+        this.year_of_publishing = year_of_publishing;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                ", year_of_publishing=" + year_of_publishing +
+                ", author=" + author +
+                ", genre='" + genre + '\'' +
+                ", publisher='" + publisher + '\'' +
+                '}';
+    }
+}
---
+++ Backend/src/main/java/com/vinternship/mcmsbackend/models/BookShop.java
@@ -0,0 +1,83 @@
+package com.vinternship.mcmsbackend.models;
+
+import java.util.Arrays;
+
+public class BookShop {
+    private String shop_name;
+    private String shop_number;
+    private String location;
+    private Book[] books;
+    private int contact_no;
+    private String email;
+
+    public BookShop(String shop_name, String shop_number, String location, Book[] books, int contact_no, String email) {
+        this.shop_name = shop_name;
+        this.shop_number = shop_number;
+        this.location = location;
+        this.books = books;
+        this.contact_no = contact_no;
+        this.email = email;
+    }
+
+    public String getShop_name() {
+        return shop_name;
+    }
+
+    public void setShop_name(String shop_name) {
+        this.shop_name = shop_name;
+    }
+
+    public String getShop_number() {
+        return shop_number;
+    }
+
+    public void setShop_number(String shop_number) {
+        this.shop_number = shop_number;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Book[] getBooks() {
+        return books;
+    }
+
+    public void setBooks(Book[] books) {
+        this.books = books;
+    }
+
+    public int getContact_no() {
+        return contact_no;
+    }
+
+    public void setContact_no(int contact_no) {
+        this.contact_no = contact_no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "BookShop{" +
+                "shop_name='" + shop_name + '\'' +
+                ", shop_number='" + shop_number + '\'' +
+                ", location='" + location + '\'' +
+                ", books=" + Arrays.toString(books) +
+                ", contact_no=" + contact_no +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
+
+
